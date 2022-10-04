3a87457779c21d7d39a483eb0eb8b027a5578eb6
Jacob Bahn <jacobabahn@gmail.com>
2022-10-04 01:02:15 -0400

    Changed some styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 const Header = () => {
   return (
     <header className="flex flex-row justify-center">
-      <div className="flex flex-row fixed w-8/12 3xl:w-1/2 items-center justify-between mt-3" id="nav">
+      <div className="flex flex-row fixed w-11/12 md:w-7/12 max-w-screen-2xl items-center justify-between mt-3" id="nav">
         <motion.div
           initial={{ x: -100, scale: 0.5 }}
           animate={{ x: 0, scale: 1.0 }}
@@ -19,7 +19,7 @@ const Header = () => {
           initial={{ x: 100, scale: 0.5 }}
           animate={{ x: 0, scale: 1.0 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-row gap-10 pr-7 float-left text-sm md:text-md"
+          className="flex flex-row gap-5 md:gap-10 pr-7 float-left text-sm md:text-md font-medium"
         >
           <button className="text-gray-400 tracking-[0.25em]">Home</button>
           <button className="text-gray-400 tracking-[0.25em]">About</button>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
         animate={{ y: 0, scale: 1.0 }}
         transition={{ duration: 0.75 }}
       >
-        <h1 className="text-5xl lg:text-6xl 2xl:text-7xl tracking-wide">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl tracking-wide">
           <span>{text}</span>
           <Cursor />
         </h1>
@@ -26,7 +26,7 @@ const Hero = () => {
           animate={{ opacity: 1.0 }}
           transition={{ delay: 3.0, duration: 2.0 }}
         >
-          <p className="uppercase text-lg pt-10 tracking-[0.5em] text-gray-400">Software Engineer</p>
+          <p className="uppercase text-sm md:text-xl py-10 tracking-[0.5em] text-gray-400">Software Engineer</p>
         </motion.div>
       </motion.div>
       <motion.div
@@ -36,7 +36,7 @@ const Hero = () => {
         className="grid grid-rows-2 gap-10 pt-12"
       >
         <motion.div
-          initial={{ x: -100 }}
+          initial={{ x: -200 }}
           animate={{ x: 0 }}
           transition={{ delay: 4.0, duration: 2.0 }}
           className="grid grid-cols-4 gap-5"
