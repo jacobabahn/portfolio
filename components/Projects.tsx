5e8befed1544d32dfbb7050fe1297c2c023cc95d
Jacob Bahn <jacobabahn@gmail.com>
2022-10-04 23:01:00 -0400

    Changed up some styling

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,17 +5,17 @@ import ConstraintBackground from '../public/ConstraintBackground.png'
 
 const Projects = () => {
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-left max-w-7xl md:flex-row px-10 justify-evenly items-center">
+    <div className="h-screen flex flex-col relative text-center md:text-left max-w-5xl px-10 justify-center gap-10 items-center">
       <div className="absolute top-32 text-gray-400 text-center text-3xl md:text-4xl tracking-[0.35em] uppercase">Projects</div>
       <motion.img
-        initial={{ x: -200, opacity: 0 }}
+        initial={{ y: -200, opacity: 0 }}
         transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1, x: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
         src={ConstraintBackground.src}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-96 h-56 rounded-lg shadow-lg object-cover md:w-7/12 md:h-auto m-5"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-96 h-56 rounded-lg shadow-lg object-cover md:w-10/12 md:h-auto m-5"
       />
-      <p className="text-xl md:text-2xl">
-        Dataflow constraint implementation using a custom built "spreadsheet".
+      <p className="text-xl md:text-2xl px-2">
+        Dataflow constraint implementation using a custom built "spreadsheet". Added support for the basic operators: +, -, *, /.
       </p>
     </div>
   )
