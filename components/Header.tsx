156b140b0a356528142a8acc4bab2a02b5c33dad
Jacob Bahn <jacobabahn@gmail.com>
2022-10-05 00:32:39 -0400

    remove logos from header on small screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,21 +6,21 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className="flex flex-row justify-center">
-      <div className="flex flex-row fixed w-11/12 md:w-9/12 2xl:w-7/12 max-w-screen-2xl items-center justify-between mt-3 z-10" id="nav">
+      <div className="flex flex-row fixed w-11/12 md:w-9/12 2xl:w-7/12 max-w-screen-2xl items-center justify-center md:justify-between mt-3 z-10" id="nav">
         <motion.div
           initial={{ x: -100, scale: 0.5 }}
           animate={{ x: 0, scale: 1.0 }}
           transition={{ duration: 0.5 }}
-          className="float-right pl-5"
+          className="pl-5 w-0 h-0 sm:h-auto sm:w-auto"
         >
-          <SocialIcon className="transition duration-300 scale-75 md:scale-100 md:hover:scale-125" fgColor="gray" bgColor="transparent" url='https://www.github.com/jacobabahn' />
-          <SocialIcon className="transition duration-300 scale-75 md:scale-100 md:hover:scale-125" fgColor="gray" bgColor="transparent" url='https://www.linkedin.com/in/jacobbahn/' />
+          <SocialIcon className="transition duration-300 scale-0 md:scale-100 md:hover:scale-125" fgColor="gray" bgColor="transparent" url='https://www.github.com/jacobabahn' />
+          <SocialIcon className="transition duration-300 scale-0 md:scale-100 md:hover:scale-125" fgColor="gray" bgColor="transparent" url='https://www.linkedin.com/in/jacobbahn/' />
         </motion.div>
         <motion.div
           initial={{ x: 100, scale: 0.5 }}
           animate={{ x: 0, scale: 1.0 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-row gap-1 pr-4 float-left text-xs md:text-md font-medium"
+          className="flex flex-row py-2 gap-1 text-xs md:text-md font-medium"
         >
           <Link href="#hero">
             <button className="navBtn">Home</button>
